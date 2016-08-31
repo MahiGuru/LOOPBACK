05801150de6f0a8c496a3cca0e2ddab1a2759ed9
mahi6535@gmail.com <mahi6535@gmail.com>
2016-08-31 17:14:12 +0530

    login page modified

--- CLIENT/Angular1/public/js/app.js
+++ CLIENT/Angular1/public/js/app.js
@@ -21,5 +21,5 @@ angular
                 controller: "reviewController"
             });
 
-        //$urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
     }]);
--- CLIENT/Angular1/public/js/controllers/loginCtrl.js
+++ CLIENT/Angular1/public/js/controllers/loginCtrl.js
@@ -1,5 +1,18 @@
 angular
     .module('GPAPP')
     .controller("loginCtrl", ['$scope', '$rootScope', '$state', '$http', "Customer", function($scope, $rootScope, $state, $http, Customer) {
-        console.log("login Ctrl created......");
+        $scope.user = 9441076540;
+        $scope.password = "mahi6535";
+        $scope.loginClick = function() {
+
+            if (typeof $scope.user == "number") {
+
+            } else if (typeof $scope.user == "string") {
+
+            }
+
+        }
+
+
+
     }]);
