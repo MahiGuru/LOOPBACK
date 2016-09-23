dbf877c4bfee25728ad11f840c1036287a679f17
mahi6535@gmail.com <mahi6535@gmail.com>
2016-09-23 14:18:37 +0530

    modal dialogue updated

--- CLIENT/Angular1/public/js/directives/modal.directive.js
+++ CLIENT/Angular1/public/js/directives/modal.directive.js
@@ -3,24 +3,15 @@ angular
     .directive("modal", function() {
         return {
             restrict: "E",
-            scope: {},
-            transclude: true,
-            template: "<div ng-transclude></div>",
-            link: function(scope, element, attrs, collapseCtrl, transclude) {
-                console.log("MODAL DIRECTIVE");
-            }
-        }
-    }).directive("modalOptions", function() {
-        return {
-            restrict: "E",
-            scope: {},
+            scope: {
+                targetid: "="
+            },
             transclude: {
-                modalheader: "?modalHeading",
+                modalHeading: "?modalHeading",
                 modalBody: "?modalBody",
                 modalFooter: "?modalFooter"
             },
             templateUrl: "public/js/directives/modal.html",
-            require: "^modal",
             link: function(scope, element, attrs, collapseCtrl, transclude) {
                 console.log("MODAL DIRECTIVE");
             }
